2be95a8b1694717a98bbbee80aece54a3b0f7e60
MD Raeed <md.raeed@student.allenisd.org>
2022-12-06 12:58:50 -0600

    Doot

---
+++ C101 Factorial [Swift]/.merlin/build/main.swift
@@ -0,0 +1,32 @@
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 5
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+//Helped by: Reynard Gunawan
+
+// Add your code below:
+
+func factorial(i: Int) -> Int {
+    print("Calculating factorial(\(i))...")
+    if (i <= 1) {
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        let l = i * factorial(i: i-1)
+        print("Non-terminal case, returning \(l).")
+        return l
+    }
+}
+
+
+// func multiplication(n: Int){
+//     if n > 1 {
+//         let l = n * factorial(i: n - 1)
+//         print("Non-terminal case, returning \(l)")
+//     }
+// }
+
+// multiplication(n: n)
+
+print("The final result is: \(factorial(i:n)).")
---
+++ C102 Fibonacci [Swift]/main.swift
@@ -0,0 +1,32 @@
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+//Helped by: Reynard Gunawan
+
+// Add your code below:
+
+func fibonacci(i: Int) -> Int {
+    print("Calculating fibonacci(\(i))...")
+    if (i <= 2) {
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        let l = fibonacci(i: i-1) + fibonacci(i: i-2) 
+        print("Non-terminal case, returning \(l).")
+        return l
+    }
+}
+
+
+// func multiplication(n: Int){
+//     if n > 1 {
+//         let l = n * factorial(i: n - 1)
+//         print("Non-terminal case, returning \(l)")
+//     }
+// }
+
+// multiplication(n: n)
+
+print("The final result is: \(fibonacci(i:n)).")
